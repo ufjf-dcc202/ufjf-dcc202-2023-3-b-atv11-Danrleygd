c9aa4c16cbcf906712bd2088ab37a43fc29e5036
DanrleyGD <danrley.dias@estudante.ufjf.br>
2023-11-21 22:30:01 -0300

    criando funçoes

---
+++ lista.js
@@ -0,0 +1,22 @@
+const lista = [
+    "Um",
+    "Dois",
+    "Três"
+];
+
+function getLista() {
+    return lista;
+}
+
+function adicionaNaLista() {
+    
+}
+
+function removeDaLista() {
+    
+}
+
+function limpaLista() {
+    
+}
+export {getLista, adicionaNaLista, removeDaLista, limpaLista};
